0fd717821d61d440fbb9c97bda1314d5101a2e71
Jahgor123 <146278690+Jahgor123@users.noreply.github.com>
2023-12-03 19:51:20 -0500

    AES Encryption Decryption
    AES Encryption Decryption

---
+++ views.py
@@ -0,0 +1,104 @@
+import secrets
+
+from django.shortcuts import render, redirect
+from .forms import MessagesForm
+from .models import Messages
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from base64 import b64encode, b64decode
+
+
+def chat(request):
+    context = {}
+
+    # create object of form
+    form = MessagesForm(request.POST or None, request.FILES or None)
+
+    # check if form data is valid
+    if form.is_valid():
+        # save the form data to model
+        form.save()
+
+    context['form'] = form
+    context['data'] = Messages.objects.all()
+    return render(request, "chatLayout.html", context)
+
+
+def ssl_trigger(request):
+    context = {}
+
+    # create object of form
+    form = MessagesForm(request.POST or None, request.FILES or None)
+
+    # check if form data is valid
+    if form.is_valid():
+        # save the form data to model
+        form.save()
+
+    # renders form on webpage
+    context['form'] = form
+    context['data'] = Messages.objects.all()
+    return render(request, "chatLayout.html", context)
+
+
+# Jahmaro Gordon -> method to encrypt message
+# want to run command : pip install pycryptodome
+def encrypt_message_to_database(message):
+    # Generate AES key to encrypt message
+    aes_key = generate_aes_keys()
+
+    # Message to encrypt
+    ciphermessage = encrypt_message(message, aes_key)
+    # for debug
+    print("Encrypted message:", b64encode(ciphermessage).decode())
+
+    # to decrypt
+    decryptedmessage = decrypt_message(message, aes_key)
+    # for debug
+    print("Decrypted message:", b64encode(ciphermessage).decode())
+
+    return message
+
+
+def generate_aes_keys():
+    # this will generate a 256 bit AES key
+    return secrets.token_bytes(32)
+
+
+# This function will pad the message
+def pad_message(message):
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(message.encode()) + padder.finalize()
+    return padded_data
+
+
+# This function encrypts a message with AES
+def encrypt_message(message, key):
+    # 128 bit IV
+    iv = secrets.token_bytes(16)
+    # algorithms.AES specifies that AES is used , key is going to be the AES key , MODE = CFB
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+    # creates the encryptor object
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(pad_message(message)) + encryptor.finalize()
+    return iv + ciphertext
+
+
+# Function to decrypt a message with AES
+def decrypt_message(ciphertext, key):
+    # get iv fron cipher text (extracts first 16 bits)
+    iv = ciphertext[:16]
+    # gets cipher text
+    ciphertext = ciphertext[16:]
+    # Creates a Cipher object from provided key, IV, and CFB mode
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    # Performs the decryption by processing the ciphertext
+    padded_message = decryptor.update(ciphertext) + decryptor.finalize()
+    # creates an unpadder for the PKCS7 padding
+    unpadder = padding.PKCS7(128).unpadder()
+    # Remove the padding to get the original message
+    message = unpadder.update(padded_message) + unpadder.finalize()
+    return message
