358111a6ebbcadee7733860c673db06be57812bb
iamjah123 <jahmaro2@gmail.com>
2023-12-03 21:56:11 -0500

    Current version of project where everything should work

--- finalProj/chat/urls.py
+++ finalProj/chat/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('chat/', views.chat, name='chat'),
-    path('login.html', views.login_view, name='login'),
+    # path('login.html', views.login_view, name='login'),
 ]
--- finalProj/chat/views.py
+++ finalProj/chat/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render, redirect
 from .forms import MessagesForm
 from .models import Messages
 
+
 def chat(request):
-    context ={}
- 
+    context = {}
+
     # create object of form
     form = MessagesForm(request.POST or None, request.FILES or None)
-     
+
     # check if form data is valid
     if form.is_valid():
         # save the form data to model
         form.save()
- 
-    context['form']= form
+
+    context['form'] = form
     context['data'] = Messages.objects.all()
     return render(request, "chatLayout.html", context)
